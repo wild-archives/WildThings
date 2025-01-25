2b07610db7e365b5d2bd50602043103bf2d71ec4
MaxtuneLee <max@xox.im>
2025-01-25 16:00:34 +0800

    feat: add dropdown menu component and accessibility rules

--- apps/wild-archive/web/app/(user)/@left/default.tsx
+++ apps/wild-archive/web/app/(user)/@left/default.tsx
@@ -1,28 +1,58 @@
-"use client";
-import Menu from "@/components/route/Menu";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { ac, Session } from "@/lib/auth-client";
-import Link from "next/link";
+'use client';
+import Menu from '@/components/route/Menu';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ac, Session } from '@/lib/auth-client';
+import { MenuIcon } from 'lucide-react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Default() {
-    const sessionInfo = ac.useSession();
-
-    return (
-        <div className="flex flex-col justify-between h-full">
-            <div>
-                <Menu />
-            </div>
-            <section id="user" className="w-full">
-                <Link href={sessionInfo.data ? "/profile" : "/auth"}>
-                    <div className="inline-flex items-center gap-4 justify-start p-4 w-full border-t border-solid hover:bg-primary-foreground cursor-pointer">
-                        <Avatar>
-                            <AvatarFallback>{sessionInfo?.data?.user?.name?.substring(0, 2) ?? '?'}</AvatarFallback>
-                        </Avatar>
-                        <span>{sessionInfo?.data?.user?.name ?? '未登录'}</span>
-                    </div>
-                </Link>
-            </section>
+  const sessionInfo = ac.useSession();
+  const router = useRouter();
+  return (
+    <div className="flex flex-col justify-between h-full">
+      <div>
+        <Menu />
+      </div>
+      <section
+        id="user"
+        className="w-full flex justify-between items-center pr-4 border-t border-solid"
+        onClick={() => !sessionInfo.data && router.push('/auth')}
+      >
+        {/* <Link href={sessionInfo.data ? '/profile' : '/auth'}> */}
+        <div className="inline-flex items-center gap-4 justify-start p-4 w-full cursor-pointer">
+          <Avatar>
+            <AvatarFallback>
+              {sessionInfo?.data?.user?.name?.substring(0, 2) ?? '?'}
+            </AvatarFallback>
+          </Avatar>
+          <span>{sessionInfo?.data?.user?.name ?? '未登录'}</span>
         </div>
-    )
+        <div className={sessionInfo.data?.user.id ? 'block' : 'hidden'}>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="bg-transparent hover:bg-primary-foreground ">
+                <MenuIcon className="text-black" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>更多</DropdownMenuLabel>
+              <DropdownMenuItem onClick={() => ac.signOut()}>
+                退出登录
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        {/* </Link> */}
+      </section>
+    </div>
+  );
 }
